48106184338b4ba496415865be994dee2fc05fbf
ClaytonAndrade <clayton_m_andrade@hotmail.com>
2024-05-02 21:20:23 -0300

    implementado CategoriaController

--- src/main/java/com/agenda_service_back/categoria/CategoriaRepository.java
+++ src/main/java/com/agenda_service_back/categoria/CategoriaRepository.java
@@ -1,6 +1,7 @@
 package com.agenda_service_back.categoria;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface CategoriaRepository extends JpaRepository<Categoria,Long> {
     // cria uma interface para ser implementada
