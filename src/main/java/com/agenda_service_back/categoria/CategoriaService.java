6766fd79305ef01024b0c02e0a25d35d6d69b08f
ClaytonAndrade <clayton_m_andrade@hotmail.com>
2024-05-02 22:02:26 -0300

    implementado CategoriaService

---
+++ src/main/java/com/agenda_service_back/categoria/CategoriaService.java
@@ -0,0 +1,48 @@
+package com.agenda_service_back.categoria;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class CategoriaService {
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
+    @Autowired
+    private CategoriaMapper categoriaMapper;
+
+    //buscar todas as categorias
+    public List<CategoriaDTO> findAll(){
+        List<Categoria> categorias =
+                categoriaRepository.findAll();
+        return categorias.stream()
+                .map(categoriaMapper::toDTO)
+                .collect(Collectors.toList());
+    }
+    //buscar uma categoria pelo id
+    public CategoriaDTO findByID(Long id){
+        Categoria categoria = categoriaRepository.findById(id)
+        .orElseThrow(()->new IllegalArgumentException("Categoria nâo encontrada"));
+        return categoriaMapper.toDTO(categoria);
+    }
+    //criando uma nova categoria
+    public CategoriaDTO create(CategoriaDTO categoriaDTO){
+        Categoria categoria = categoriaMapper.toEntity(categoriaDTO);
+        categoria = categoriaRepository.save(categoria);
+        return categoriaMapper.toDTO(categoria);
+    }
+    //update categoria
+    public CategoriaDTO update(Long id,CategoriaDTO categoriaDTO){
+        Categoria categoria = categoriaRepository.findById(id)
+                .orElseThrow(()->new IllegalArgumentException("Categoria não"));
+        categoriaMapper.updateEntity(categoriaDTO,categoria);
+        categoria = categoriaRepository.save(categoria);
+        return categoriaMapper.toDTO(categoria);
+    }
+    public void deleteById(Long id){
+        categoriaRepository.deleteById(id);
+    }
+}
